cea07776cc957f4cb423a2793f0dcd9768b31624
sibeliouss <sibelmozturk@hotmail.com>
2024-08-09 13:29:41 +0300

    Home and Login Controller Added

---
+++ WebMVC/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+using System.Security.Claims;
+using Application.Features.Auth.Login;
+using MediatR;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebMVC.Controllers;
+
+public class AuthController : Controller
+{
+    private readonly IMediator _mediator;
+
+    public AuthController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    // GET
+    public IActionResult Login()
+    {
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginCommand model, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(model, cancellationToken);
+
+        TempData["UserNameOrEmail"] = model.UserNameOrEmail;
+        TempData["Password"] = model.Password;
+
+        List<Claim> claims = new()
+        {
+            new Claim("Name", "Sibel Öztürk")
+        };
+
+        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        var authProperties = new AuthenticationProperties();
+        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+
+        return RedirectToAction("Index", "Home");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync();
+        return RedirectToAction("Login", "Auth");
+    }
+}
