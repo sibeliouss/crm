2f4efe3d80088b05e6ac4d740b64e5272bd09e5c
sibeliouss <sibelmozturk@hotmail.com>
2024-08-16 20:21:25 +0300

    .

--- Application/ApplicationServiceRegistration.cs
+++ Application/ApplicationServiceRegistration.cs
@@ -1,4 +1,5 @@
 using Application.Features.Customers.Profiles;
+using Application.Services;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,6 +9,7 @@ public static class ApplicationServiceRegistration
 {
     public static void AddApplication(this IServiceCollection services)
     {
+        services.AddScoped<ICustomerService, CustomerService>();
         var assembly = typeof(ApplicationServiceRegistration).Assembly;
         services.AddMediatR(config => config.RegisterServicesFromAssembly(assembly));
         services.AddValidatorsFromAssembly(assembly);
