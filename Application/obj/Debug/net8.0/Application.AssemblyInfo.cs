e3e513f1580a3cfc79386a0d35da97145a4f8944
sibeliouss <sibelmozturk@hotmail.com>
2024-08-19 22:17:17 +0300

    .

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c2f176bf428cd2680bd00704a3052b171cc9ade")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5077c279bc4dcdc1f67d058e6b7e1b241078dec9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
